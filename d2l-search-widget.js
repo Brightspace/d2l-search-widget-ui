5af93294b7c0ba417fb3fbf60cd946426687f604
Margaree Peacocke <margaree.p@gmail.com>
2018-11-28 14:29:17 -0500

    Polymer 3 Conversion

---
+++ d2l-search-widget.js
@@ -0,0 +1,40 @@
+/**
+`d2l-search-widget`
+Polymer-based web component that searches things.
+
+@demo demo/index.html
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import '../@polymer/polymer/polymer-legacy.js';
+
+import '../d2l-fetch/d2l-fetch.js';
+import '../d2l-icons/d2l-icons.js';
+import '../@polymer/iron-input/iron-input.js';
+import './localize-behavior.js';
+import './d2l-search-widget-behavior.js';
+import './d2l-search-widget-styles.js';
+import { Polymer } from '../@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '../@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
+		<style include="d2l-search-widget-styles"></style>
+		<iron-input bind-value="{{_searchInput}}">
+			<input value="{{_searchInput}}" placeholder="[[placeholderText]]" on-keydown="_onSearchInputKeyPressed">
+			
+		</iron-input>
+		<button type="button" on-tap="_onButtonClick" aria-label$="[[searchButtonLabel]]">
+			<d2l-icon icon="d2l-tier1:search"></d2l-icon>
+		</button>
+`,
+
+  is: 'd2l-search-widget',
+
+  behaviors: [
+	  D2L.PolymerBehaviors.SearchWidget.LocalizeBehavior,
+	  D2L.PolymerBehaviors.SearchWidgetBehavior
+  ]
+});
