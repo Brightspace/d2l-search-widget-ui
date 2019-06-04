41d6f82a38d1695c0128c7e913a45de23d06874d
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-06-04 13:54:00 -0400

    Enhance the results event by also sending the input that was searched

--- d2l-search-widget-behavior.js
+++ d2l-search-widget-behavior.js
@@ -128,7 +128,10 @@ D2L.PolymerBehaviors.SearchWidgetBehavior = {
 	},
 
 	_onSearchResponse: function(searchResponse) {
-		this.fire('d2l-search-widget-results-changed', searchResponse || {});
+		var detail = {};
+		detail.searchValue = this._getSearchInput().value;
+		detail.searchResponse = searchResponse;
+		this.fire('d2l-search-widget-results-changed', detail);
 	},
 
 	_onSearchUrlChanged: function(url) {
